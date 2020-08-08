ad56faffe24f09302148953d9ab8eebd2a65948d
gmocontreras-dev <gmocontreras@gmail.com>
2020-08-08 18:22:03 -0400

    Add files via upload

---
+++ RequestInterceptorAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class RequestInterceptorAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var method = filterContext.HttpContext.Request.Method;
+            var form = filterContext.HttpContext.Request.Form;
+            var query = filterContext.HttpContext.Request.Query;
+            var headers = filterContext.HttpContext.Request.Headers;
+            var controller = filterContext.ActionDescriptor.DisplayName;
+
+        }
+    }
+}
